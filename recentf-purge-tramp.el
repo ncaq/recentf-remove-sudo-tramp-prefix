9d16b6ba66e89f60c06c7680da1a36e32a6011c1
ncaq <ncaq@ncaq.net>
2013-08-23 23:35:58 +0900

    やっと問題解決したっぽい

--- recentf-purge-tramp.el
+++ recentf-purge-tramp.el
@@ -1,11 +1,15 @@
 (require 'recentf)
 (require 'recentf-ext)
+(defvar current-dir (file-name-directory load-file-name))
+
 (defun purge ()
   (recentf-save-list)
-  (shell-command (expand-file-name "~/.emacs.d/package.d/recentf-purge-tramp/a.out"))
+  (shell-command (expand-file-name (concat current-dir "a.out")))
   (recentf-load-list))
+
 (defun recentf-purge-tramp-stand-by ()
   (add-hook 'kill-emacs-hook 'purge))
+
 (defun recentf-purge-tramp ()
   (purge)
   (recentf-purge-tramp-stand-by))
