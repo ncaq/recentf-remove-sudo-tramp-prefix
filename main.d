ee2b215bdc926c6ae93edeb35b1e836b23f0600d
ncaq <nyrigadake38@gmail.com>
2013-10-03 07:25:30 +0900

    Performance Tuning

--- main.d
+++ main.d
@@ -5,16 +5,12 @@ import std.path;
 void main()
 {
 	immutable default_recentf = expandTilde("~/.recentf");	///チルダ展開
-	string filename;	///リンク取得
-	if(isSymlink(default_recentf))
-	{
-		filename = expandTilde("~/") ~ readLink(default_recentf);
-	}
-	else
-	{
-		filename = default_recentf;
-	}
-	auto oldstring = readText(filename);
+
+	//immutableにしたかったため一行に詰め込むことに.
+	//Symbolic-Linkの場合,本当のファイルを追うようになっています
+	immutable filename = (isSymlink(default_recentf)) ? (expandTilde("~/") ~ readLink(default_recentf)) : default_recentf;
+
+	immutable oldstring = readText(filename);
 	
 	/**
 	   "/sudo:root@gentoo-z9-u3:/etc/portage/package.use"
@@ -22,8 +18,8 @@ void main()
 	   "/etc/portage/package.use"
 	   にする,などを想定
 	*/
-	static auto r = regex(`/sudo:root@.+:`,"gm");
-	auto newstring = replace(oldstring,r,"");
+	auto r = ctRegex!(`/sudo:root@.+:`,"gm");//機械語正規表現,ctRegex
+	immutable newstring = replace(oldstring,r,"");//なんでimmutableの正規表現受付ない仕様なんだろう…?
 
 	std.file.write(filename,newstring);
 	//重複とかはEmacs側が何とかしてくれるだろ…多分…
