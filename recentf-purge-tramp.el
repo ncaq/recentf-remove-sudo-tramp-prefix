3ea8eb4ce26b429b3caaee2ab3ee6069f27be505
ncaq <ncaq@ncaq.net>
2013-12-24 00:09:08 +0900

    重複が発生することが判明

--- recentf-purge-tramp.el
+++ recentf-purge-tramp.el
@@ -4,7 +4,8 @@
   (replace-regexp-in-string "^/sudo:root@.+:" "" source))
 
 (defun purge ()
-  (setq recentf-list (mapcar 'delete-sudo-tramp recentf-list)))
+  (setq recentf-list (mapcar 'delete-sudo-tramp recentf-list))
+  (recentf-cleanup))
 
 (add-hook 'after-init-hook 'purge)
 
