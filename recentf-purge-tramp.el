427aebc6ef2eca614fa91dc6ace7c08fede8ae58
ncaq <ncaq@ncaq.net>
2014-03-21 00:17:32 +0900

    cleanするのは無駄だからやめた

--- recentf-purge-tramp.el
+++ recentf-purge-tramp.el
@@ -4,8 +4,7 @@
   (replace-regexp-in-string "^/sudo:root@.+:" "" source))
 
 (defun purge ()
-  (setq recentf-list (mapcar 'delete-sudo-tramp recentf-list))
-  (recentf-cleanup))
+  (setq recentf-list (mapcar 'delete-sudo-tramp recentf-list)))
 
 (add-hook 'after-init-hook 'purge)
 
