40b53d6508e6dc3f72bc6a0682edaf389b86bdb7
ncaq <ncaq@ncaq.net>
2015-03-08 13:47:55 +0900

    clean helm-source-recentf

--- recentf-remove-sudo-tramp-prefix.el
+++ recentf-remove-sudo-tramp-prefix.el
@@ -1,11 +1,20 @@
 ;; -*- lexical-binding: t -*-
+
+(require 'eieio)
 (require 'recentf)
 
 (defun remove-prefix (source)
   (replace-regexp-in-string "^/sudo:root@.+:" "" source))
 
+(defun recentf-list-nothing-sudo ()
+  (mapcar 'remove-prefix recentf-list))
+
+(with-eval-after-load 'helm-files
+  (setf (cdr (assoc 'candidates helm-source-recentf)) 'recentf-list-nothing-sudo)
+  )
+
 (defun remove-from-recentf-list ()
-  (setq recentf-list (mapcar 'remove-prefix recentf-list))
+  (setq recentf-list (recentf-list-nothing-sudo))
   (recentf-cleanup))
 
 (if recentf-mode
