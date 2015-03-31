32677e1dc408b4344ff52662115cfbac41c2822e
ncaq <ncaq@ncaq.net>
2015-03-31 22:49:20 +0900

    added: remove duplicate element from helm list

--- recentf-remove-sudo-tramp-prefix.el
+++ recentf-remove-sudo-tramp-prefix.el
@@ -5,12 +5,21 @@
 (defun remove-prefix (source)
   (replace-regexp-in-string "^/sudo:root@.+:" "" source))
 
+(defun uniq (xs)
+  "omit repeated lines only adjacent"
+  (reduce (lambda (x a)
+            (if (string-equal x (car a))
+                a
+              (cons x a)))
+          xs
+          :initial-value '()
+          :from-end t))
+
 (defun recentf-list-nothing-sudo ()
-  (mapcar 'remove-prefix recentf-list))
+  (uniq (mapcar 'remove-prefix recentf-list)))
 
 (with-eval-after-load 'helm-files
-  (setf (cdr (assoc 'candidates helm-source-recentf)) 'recentf-list-nothing-sudo)
-  )
+  (setf (cdr (assoc 'candidates helm-source-recentf)) 'recentf-list-nothing-sudo))
 
 (defun remove-from-recentf-list ()
   (setq recentf-list (recentf-list-nothing-sudo)))
