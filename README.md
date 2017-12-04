f102bc88ea99e69aed0eae8b6e7b809989b9e45c
ncaq <ncaq@ncaq.net>
2017-12-04 17:57:08 +0900

    changed: LICENSE, package-lint, checkdoc

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-recentf-remove-sudo-tramp-prefix
-===================
+# recentf-remove-sudo-tramp-prefix
 
-```
+```txt
 /sudo:root@localhost:/etc/portage/package.use
 ↓
 /etc/portage/package.use
 ```
-
-Public Domain
--- recentf-remove-sudo-tramp-prefix.el
+++ recentf-remove-sudo-tramp-prefix.el
@@ -1,9 +1,24 @@
-;; -*- lexical-binding: t -*-
+;;; recentf-remove-sudo-tramp-prefix.el --- Normalise recentf history -*- lexical-binding: t -*-
+
+;; Author: ncaq <ncaq@ncaq.net>
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.4"))
+;; URL: https://github.com/ncaq/recentf-remove-sudo-tramp-prefix
+
+;;; Commentary:
+
+;; recentf save duplicate history, for instance.
+;; "/sudo:root@akaza:/usr/share/emacs/24.5/lisp/net/tramp.el"
+;; "/usr/share/emacs/24.5/lisp/net/tramp.el"
+;; This Package normalise to after.
+
+;;; Code:
 
 (require 'recentf)
 (require 'tramp)
 
-(defun remove-sudo (x)
+(defun recentf-remove-sudo-tramp-prefix-remove-sudo (x)
+  "Remove sudo from path.  Argument X is path."
   (if (tramp-tramp-file-p x)
       (let ((tx (tramp-dissect-file-name x)))
         (if (string-equal "sudo" (tramp-file-name-method tx))
@@ -11,9 +26,13 @@
           x))
     x))
 
-(defun delete-sudo-from-recentf-list ()
-  (setq recentf-list (mapcar 'remove-sudo recentf-list)))
+;;;###autoload
+(defun recentf-remove-sudo-tramp-prefix-delete-sudo-from-recentf-list ()
+  "Do `mapcar' `recentf-sudo' to `recentf-list'."
+  (setq recentf-list (mapcar 'recentf-remove-sudo-tramp-prefix-remove-sudo recentf-list)))
 
-(advice-add 'recentf-cleanup :before 'delete-sudo-from-recentf-list)
+(advice-add 'recentf-cleanup :before 'recentf-remove-sudo-tramp-prefix-delete-sudo-from-recentf-list)
 
 (provide 'recentf-remove-sudo-tramp-prefix)
+
+;;; recentf-remove-sudo-tramp-prefix.el ends here
