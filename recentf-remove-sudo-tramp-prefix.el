f62724a11a1de858f400494713d895fbfacf0f35
ncaq <ncaq@ncaq.net>
2018-01-29 18:35:19 +0900

    chnaged: Commentaryの内容を修正

--- recentf-remove-sudo-tramp-prefix.el
+++ recentf-remove-sudo-tramp-prefix.el
@@ -7,10 +7,11 @@
 
 ;;; Commentary:
 
-;; recentf save duplicate history, for instance.
+;; recentf saves both the file path with sudo prefix and the unfounded file path.
+;; for instance.
 ;; "/sudo:root@akaza:/usr/share/emacs/24.5/lisp/net/tramp.el"
 ;; "/usr/share/emacs/24.5/lisp/net/tramp.el"
-;; This Package normalise to after.
+;; This package normalizes the history of recentf to those without sudo prefix.
 
 ;;; Code:
 
